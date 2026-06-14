8eee2e8ebc87462d146fd9ef50c12690ae318ebe
dvaustria741-a11y <dvaustria741@gmail.com>
2026-06-14 08:35:57 +0800

    Add EntityAccessor mixin for fallDistance access

---
+++ src/main/java/com/zen/automlg/mixin/EntityAccessor.java
@@ -0,0 +1,12 @@
+package com.zen.automlg.mixin;
+
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(Entity.class)
+public interface EntityAccessor {
+
+    @Accessor("fallDistance")
+    float automlg$getFallDistance();
+}
